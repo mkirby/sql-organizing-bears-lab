9b7e844f5605721bf7747a4fd4fc28e35a772350
Matthew Kirby <matt.kirby@yahoo.com>
2020-09-22 19:52:42 -0400

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, "M", "Brown", "Rowdy", 1),
+('Rowdy', 5, 'F', "Black", "Sneaky", 1),
+('Tabitha', 3, 'F', 'Dark Brown', "Grouchy", 1),
+('Sergeant Brown', 12, 'F', 'Tan', 'Strict', 1),
+('Melissa', 8, 'F', 'Brown', 'Goofy', 1),
+('Grinch', 1, 'M', 'Black', 'Curious', 1),
+('Wendy', 1, 'F', 'Black', 'Reserved', 1),
+(NULL, NULL, NULL, NUll, 'KILLER', 1);
